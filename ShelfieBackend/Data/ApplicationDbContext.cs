5d42c1b21bd61fc8f0cbf7b853f2c1b11505cb70
qwiklly <kolymarchenko2004@gmail.com>
2025-02-15 21:02:58 +0300

    feat: add ApplicationDbContext

---
+++ ShelfieBackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShelfieBackend.Models;
+
+namespace ShelfieBackend.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Medication> Medications { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductHistory> ProductHistories { get; set; }
+    }
+}
