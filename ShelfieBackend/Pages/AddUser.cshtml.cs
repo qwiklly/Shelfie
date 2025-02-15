a8deb5667cff2a76491d8612d34ec4973235c1d9
qwiklly <kolymarchenko2004@gmail.com>
2025-02-15 21:11:41 +0300

    feat: add pages

---
+++ ShelfieBackend/Pages/AddUser.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShelfieBackend.DTOs;
+using ShelfieBackend.Services;
+
+namespace ShelfieBackend.Pages
+{
+    public class AddUserModel : PageModel
+    {
+        [BindProperty]
+        public RegisterDTO Register { get; set; } = new RegisterDTO();
+
+        public bool IsUserAdded { get; set; } = false;
+
+        private readonly IApplicationService _accountService;
+
+        public AddUserModel(IApplicationService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public void OnGet()
+        {
+            TempData["SuccessMessage"] = null;
+            TempData["ErrorMessage"] = null;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var response = await _accountService.RegisterAsync(Register);
+
+            if (!response.Flag)
+            {
+                TempData["ErrorMessage"] = response.Message;
+                return Page();
+            }
+
+            IsUserAdded = true;  
+            return Page(); 
+        }
+    }
+}
---
+++ ShelfieBackend/Pages/Registration.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShelfieBackend.DTOs;
+using ShelfieBackend.Services;
+
+namespace ShelfieBackend.Pages
+{
+    public class RegistrationModel : PageModel
+    {
+        [BindProperty]
+        public RegisterDTO Register { get; set; } = new RegisterDTO();
+
+        public bool IsUserAdded { get; set; } = false;
+
+        private readonly IApplicationService _accountService;
+
+        public RegistrationModel(IApplicationService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public void OnGet()
+        {
+            // clear temp after reloading page
+            TempData["SuccessMessage"] = null;
+            TempData["ErrorMessage"] = null;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+			if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+			var response = await _accountService.RegisterAsync(Register);
+
+            if (!response.Flag)
+            {
+                TempData["ErrorMessage"] = response.Message;
+                return Page();
+            }
+
+            IsUserAdded = true;  
+            return Page(); 
+        }
+    }
+}
