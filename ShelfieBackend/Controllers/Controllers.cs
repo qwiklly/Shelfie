c39f14f07bdf28da98f168ec7b3b08c22d6819c4
qwiklly <kolymarchenko2004@gmail.com>
2025-02-15 21:07:30 +0300

    feat: add controllers for account operations

---
+++ ShelfieBackend/Controllers/Controllers.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShelfieBackend.DTOs;
+using static ShelfieBackend.Responses.CustomResponses;
+using Swashbuckle.AspNetCore.Annotations;
+using ShelfieBackend.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace ShelfieBackend.Controllers
+{
+	[Route("api/application")]
+	[ApiController]
+	public class ApplicationController(IAccount _accountrepo) : ControllerBase
+	{
+        [Authorize]
+        [HttpGet("getUsers")]
+		[SwaggerOperation(
+			Summary = "Get all users",
+			Description = "Retrieve a list of all users from the database."
+		)]
+		public async Task<ActionResult<BaseResponse>> GetUsersAsync()
+		{
+			var result = await _accountrepo.GetUsersAsync();
+			return Ok(result);
+		}
+
+        [HttpGet("getUser")]
+		[SwaggerOperation(
+			Summary = "Get one user by id",
+			Description = "Retrieve a single user from the database using their email address."
+		)]
+		public async Task<ActionResult<BaseResponse>> GetUserAsync(string email)
+		{
+			var result = await _accountrepo.GetUserAsync(email);
+			return Ok(result);
+		}
+
+		[HttpPost("register")]
+		[SwaggerOperation(
+			Summary = "Register a new user",
+			Description = "Registers a new user."
+		)]
+        public async Task<ActionResult<RegisterResponse>> RegisterAsync([FromBody] RegisterDTO model)
+        {
+            var currentUser = HttpContext.User; // Получаем текущего пользователя из контекста
+
+            var result = await _accountrepo.RegisterAsync(model, currentUser);
+            return Ok(result);
+        }
+
+
+        [HttpPost("login")]
+		[SwaggerOperation(
+			Summary = "Login a user",
+			Description = "Authenticate a user with email and password."
+		)]
+		public async Task<ActionResult<LoginResponse>> LoginAsync(LoginDTO model)
+		{
+			var result = await _accountrepo.LoginAsync(model);
+			return Ok(result);
+		}
+
+        [Authorize]
+        [HttpDelete("deleteUser")]
+		[SwaggerOperation(
+			Summary = "Delete a user",
+			Description = "Delete a user."
+		)]
+		public async Task<ActionResult<BaseResponse>> DeleteUserAsync(DeleteUserDTO model)
+		{
+			var result = await _accountrepo.DeleteUserAsync(model);
+			return Ok(result);
+		}
+
+		[HttpPut("updateUser/{email}")]
+		[SwaggerOperation(
+			Summary = "Update a user",
+			Description = "Update a user's information, such as name, role, or password."
+		)]
+		public async Task<ActionResult<BaseResponse>> UpdateUserAsync(string email, RegisterDTO model)
+		{
+            var currentUser = HttpContext.User; // Получаем текущего пользователя из контекста
+            var result = await _accountrepo.UpdateUserAsync(email, model, currentUser);
+			return Ok(result);
+		}
+	}
+}
