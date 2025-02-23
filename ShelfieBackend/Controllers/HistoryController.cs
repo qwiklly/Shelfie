0684ca2ccf5853381fd3d120a52769b8c9ff2662
qwiklly <kolymarchenko2004@gmail.com>
2025-02-23 18:03:02 +0300

    feat: add product and product history controllers

---
+++ ShelfieBackend/Controllers/HistoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using ShelfieBackend.Repositories;
+using ShelfieBackend.DTOs;
+
+namespace ShelfieBackend.Controllers
+{
+    [Route("api/application")]
+    [ApiController]
+    public class HistoryController(IProductRepo _productRepo) : ControllerBase
+    {
+
+        [Authorize]
+        [HttpGet("getAllHistory")]
+        [SwaggerOperation(Summary = "Get all history", Description = "Retrieves all product history.")]
+        public async Task<ActionResult<List<ProductHistoryDTO>>> GetAllHistoryAsync()
+        {
+            var currentUser = HttpContext.User;
+            var result = await _productRepo.GetAllHistory(currentUser);
+            return Ok(result);
+        }
+    }
+}
---
+++ ShelfieBackend/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShelfieBackend.DTOs;
+using static ShelfieBackend.Responses.CustomResponses;
+using Swashbuckle.AspNetCore.Annotations;
+using ShelfieBackend.Repositories;
+
+namespace ShelfieBackend.Controllers
+{
+    [Route("api/application")]
+    [ApiController]
+    public class ProductController(IProductRepo _productRepo) : ControllerBase
+    {
+
+        [Authorize]
+        [HttpPost("add")]
+        [SwaggerOperation(Summary = "Add a new product", Description = "Adds a new product to the inventory.")]
+        public async Task<ActionResult<BaseResponse>> AddProductAsync([FromBody] AddProductDTO model)
+        {
+            var currentUser = HttpContext.User;
+            var result = await _productRepo.AddProductAsync(model, currentUser);
+            return Ok(result);
+        }
+        [Authorize]
+        [HttpGet("getAll")]
+        [SwaggerOperation(Summary = "Get all products", Description = "Retrieves all products.")]
+        public async Task<ActionResult<BaseResponse>> GetProductsAsync()
+        {
+            var currentUser = HttpContext.User;
+            var result = await _productRepo.GetProductsAsync(currentUser);
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpGet("get/{name}")]
+        [SwaggerOperation(Summary = "Get product by name", Description = "Retrieves a product by its name.")]
+        public async Task<ActionResult<BaseResponse>> GetProductAsync(string name)
+        {
+            var currentUser = HttpContext.User;
+            var result = await _productRepo.GetProductAsync(name, currentUser);
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpPut("update/{currentName}")]
+        [SwaggerOperation(Summary = "Update a product", Description = "Updates a product's details.")]
+        public async Task<ActionResult<BaseResponse>> UpdateProductAsync(string currentName, [FromBody] UpdateProductDTO model)
+        {
+            var currentUser = HttpContext.User;
+            var result = await _productRepo.UpdateProductAsync(currentName, model, currentUser);
+            return Ok(result);
+        }
+
+
+        [Authorize]
+        [HttpDelete("delete/{name}")]
+        [SwaggerOperation(Summary = "Delete a product", Description = "Deletes a product by its name.")]
+        public async Task<ActionResult<BaseResponse>> DeleteProductAsync(string name)
+        {
+            var currentUser = HttpContext.User;
+            var result = await _productRepo.DeleteProductAsync(name, currentUser);
+            return Ok(result);
+        }
+    }
+}
