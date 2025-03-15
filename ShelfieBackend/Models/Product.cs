79fda97efdd094dfe5d59eb863740b02e274128c
qwiklly <kolymarchenko2004@gmail.com>
2025-03-15 23:26:33 +0300

    feat: add Weight, WeightUnit and description model

--- ShelfieBackend/Models/Product.cs
+++ ShelfieBackend/Models/Product.cs
@@ -6,8 +6,9 @@
         public string Name { get; set; } = string.Empty;
         public string Creator { get; set; } = string.Empty;
         public int CategoryId { get; set; }
-        public string Description {  get; set; } = string.Empty;
-        public double Weight { get; set; } 
+        public string? Description {  get; set; } 
+        public double Weight { get; set; }
+        public string WeightUnit { get; set; } = string.Empty;
         public Category Category { get; set; } = null!;
         public DateOnly? ExpirationDate { get; set; }
         public int Quantity { get; set; }
