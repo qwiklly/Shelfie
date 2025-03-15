9151883aab781ae10d961c5330ad520bdd262a77
qwiklly <kolymarchenko2004@gmail.com>
2025-03-15 23:24:51 +0300

    feat: add categoryFieldValue model

---
+++ ShelfieBackend/Models/CategoryFieldValue.cs
@@ -0,0 +1,9 @@
+﻿namespace ShelfieBackend.Models
+{
+    public class CategoryFieldValue
+    {
+        public int Id { get; set; }
+        public int CategoryFieldId { get; set; } 
+        public string Value { get; set; } = string.Empty;  
+    }
+}
