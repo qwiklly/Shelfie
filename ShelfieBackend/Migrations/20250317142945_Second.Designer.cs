f36bc4ee5c7637b684f01e21249392f6bde1916d
qwiklly <kolymarchenko2004@gmail.com>
2025-03-22 19:34:39 +0300

    feat: add new migrations

--- ShelfieBackend/Migrations/20250317142945_Second.Designer.cs
+++ ShelfieBackend/Migrations/20250317142945_Second.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ShelfieBackend.Data;
@@ -11,9 +12,11 @@ using ShelfieBackend.Data;
 namespace ShelfieBackend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250317142945_Second")]
+    partial class Second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -116,9 +119,6 @@ namespace ShelfieBackend.Migrations
                         .HasColumnType("integer");
 
                     b.Property<int>("CategoryId")
-                        .HasColumnType("integer");
-
-                    b.Property<int>("RecordId")
                         .HasColumnType("integer");
 
                     b.Property<string>("Value")
---
+++ ShelfieBackend/Migrations/20250318150754_Third.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShelfieBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Third : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RecordId",
+                table: "CategoryFieldValues",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecordId",
+                table: "CategoryFieldValues");
+        }
+    }
+}
