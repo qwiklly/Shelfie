0a7a33d4dce1895ae2bec24561f6cf3c31f83f21
qwiklly <kolymarchenko2004@gmail.com>
2025-03-22 19:35:06 +0300

    refactor: remove comment

--- ShelfieBackend/DTOs/GetCategoriesDTO.cs
+++ ShelfieBackend/DTOs/GetCategoriesDTO.cs
@@ -1,12 +1,10 @@
-﻿using ShelfieBackend.Models;
-
-namespace ShelfieBackend.DTOs
+﻿namespace ShelfieBackend.DTOs
 {
     public class GetCategoriesDTO
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty; // "Book", "Product", "Medication", "Custom"
-        public int? UserId { get; set; } // Null если это предопределённая категория
+        public int? UserId { get; set; } 
     }
 }
