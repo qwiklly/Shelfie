4c482a23cc98513363ff8fffc177ad3872d6ec1b
qwiklly <kolymarchenko2004@gmail.com>
2025-03-22 19:36:24 +0300

    feat: add new DTO

---
+++ ShelfieBackend/DTOs/UpdateUserDTO.cs
@@ -0,0 +1,12 @@
+﻿using ShelfieBackend.States;
+
+namespace ShelfieBackend.DTOs
+{
+    public class UpdateUserDTO
+    {
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public UserRole? Role { get; set; }
+    }
+}
